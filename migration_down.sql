cd6124d81b52dc87c81dc5993ff2aae2f52fd258
Hgo92 <hugo_monier@yahoo.fr>
2025-12-17 15:48:33 +0100

    ajout projects

--- migration_down.sql
+++ migration_down.sql
@@ -1,3 +1,3 @@
-DROP TABLE themes, resources, resources_skills, skills CASCADE;
+DROP TABLE IF EXISTS themes, resources, resources_skills, skills, projects, projects_skills, projects_themes CASCADE;
 
 DROP TYPE resources_types;
--- src/server.js
+++ src/server.js
@@ -59,13 +59,45 @@ res.json(`L'élément ${id[0]} a été supprimé de themes`)
 app.delete("/deleteSkills/:id", async function (req, res) {
     const { id } = req.params;
     await pool.query("DELETE FROM resources_skills WHERE skill_id = $1", [id]);
-    await pool.query("DELETE FROM skills WHERE id=$1", [id])
+    await pool.query("DELETE FROM skills WHERE id=$1", [id]);
 res.json(`L'élément ${id[0]} a été supprimé de skills`)
-})
+});
 
 app.delete("/deleteResSkils/:id", async function (req, res) {
     const { id } = req.params;
     await pool.query("DELETE FROM resources_skills WHERE id=$1", [id])
 res.json(`L'élément ${id[0]} a été supprimé de resources_skills`)
+});
+
+// Bonus 
+// Ajouter une table projects et la remplir
+
+app.post("/projectsUP", async function (req, res) {
+    await pool.query("CREATE TABLE projects (id SERIAL PRIMARY KEY, title TEXT NOT NULL, description TEXT NOT NULL)");
+    await pool.query("CREATE TABLE project_skills (project_id INT REFERENCES projects (id), skill_id INT REFERENCES skills (id));");
+    await pool.query("CREATE TABLE project_themes (project_id INT REFERENCES projects (id), theme_id INT REFERENCES themes (id));");
+    res.json('La table projects a été créée');
+});
+
+app.post("/fillProjects", async function (req, res) {
+    await pool.query("INSERT INTO projects (title, description) VALUES ('Adatabase', 'Projet autour de SQL'),('Adapi', 'Mise en place d''une API REST'), ('Adataviz', 'Mise en forme d''une API exploitable par les utilisateurs')");
+    await pool.query("INSERT INTO project_skills (project_id, skill_id) VALUES (1, 3), (1,4), (2,3), (2,4), (3,1), (3,3), (3,4), (3,5)");
+    await pool.query("INSERT INTO project_themes (project_id, theme_id) VALUES (1,2), (1,3), (2,2), (2,3), (3,1), (3,2), (3,3)");
+    res.json('Les tables projects, project_skills et project_themes ont été remplies');
 })
+
+app.get("/projects", async function (req, res) {
+  const { rows } = await pool.query("SELECT * FROM projects");
+res.json(rows);});
+
+app.get("/projects/:id", async function (req, res) {
+    const { id } = req.params;
+    const { rows } = await pool.query("SELECT * FROM projects WHERE id=$1", [id]);
+res.json(rows);});
+
+app.post("/add-adaquiz", async function (req, res) {
+    await pool.query("INSERT INTO projects (title, description) VALUES ('Adaquiz', 'Création d''un quiz')");
+    await pool.query("INSERT INTO project_skills (project_id, skill_id) VALUES (4,4)");
+    await pool.query("INSERT INTO project_themes (project_id, theme_id) VALUES (4,1)");
+res.json('Le projet Adaquiz a été ajouté à la table projects');});
 
